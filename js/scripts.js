4e1f8e763a82ddbed682ff3b4cecbce8b2f522b2
Nicholas Gunner <support@getbeyondwords.com>
2018-05-20 08:38:03 -0600

    an initial vr

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// Toggle the chapters menu
 $( "#chapters-menu-toggle" ).click(function() {
   $( "div.chapters-menu" ).toggleClass( "show" );
 });
+
+// Load the VR View
+window.addEventListener('load', onVrViewLoad);
+function onVrViewLoad() {
+  var vrView = new VRView.Player('#vrview', {
+    image: 'img/sean-trogon.jpg'
+  });
+}
